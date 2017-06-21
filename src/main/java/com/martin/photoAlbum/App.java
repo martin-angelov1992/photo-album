1492b3df6c6fb92a9ac89e296c5ce29e2ea9f7fb
martin.angelov <martin.angelov@mobisystems.com>
2017-06-21 19:14:01 +0300

    some unfinished stuff

--- src/main/java/com/martin/photoAlbum/App.java
+++ src/main/java/com/martin/photoAlbum/App.java
@@ -1,13 +1,52 @@
 package com.martin.photoAlbum;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.eclipse.jetty.server.Server;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting Crunchify's Embedded Jersey HTTPServer...\n");
+        HttpServer crunchifyHTTPServer = createHttpServer();
+        crunchifyHTTPServer.start();
+        System.out.println(String.format("\nJersey Application Server started with WADL available at " + "%sapplication.wadl\n", getCrunchifyURI()));
+        System.out.println("Started Crunchify's Embedded Jersey HTTPServer Successfully !!!");
+    }
+ 
+    private static HttpServer createHttpServer() throws IOException {
+        ResourceConfig crunchifyResourceConfig = new PackagesResourceConfig("com.crunchify.tutorial");
+        // This tutorial required and then enable below line: http://crunchify.me/1VIwInK
+        //crunchifyResourceConfig.getContainerResponseFilters().add(CrunchifyCORSFilter.class);
+        return HttpServerFactory.create(getCrunchifyURI(), crunchifyResourceConfig);
+    }
+ 
+    private static URI getCrunchifyURI() {
+        return UriBuilder.fromUri("http://" + crunchifyGetHostName() + "/").port(8085).build();
+    }
+ 
+    private static String crunchifyGetHostName() {
+        String hostName = "localhost";
+        try {
+            hostName = InetAddress.getLocalHost().getCanonicalHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return hostName;
     }
 }
