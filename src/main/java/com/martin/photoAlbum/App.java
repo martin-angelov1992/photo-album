f87e6f8bdd301ddd726a16945940baedc74fa507
martin.angelov <martin.angelov@mobisystems.com>
2017-06-21 14:54:56 +0300

    initial commit

---
+++ src/main/java/com/martin/photoAlbum/App.java
@@ -0,0 +1,13 @@
+package com.martin.photoAlbum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
