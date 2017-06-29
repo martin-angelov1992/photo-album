7dd7276194efd00621802ead860fdc0c80551088
martin.angelov <martin.angelov@mobisystems.com>
2017-06-29 19:36:46 +0300

    more work on front-end

--- front-end/js/categories.js
+++ front-end/js/categories.js
@@ -1,17 +1,27 @@
 function populateCategories() {
 	var categories = getCategoryTree();
-	
-	buildCategoryTrees(categories);
+
+	var categoryTree = buildCategoryTrees(categories);
+	$("#categories").html(categoryTree);
 }
 
 function buildCategoryTrees(categories) {
+	var wrapper = $("#categoryTemplate").clone();
 	if (categories.length == 0) {
 		return;
 	}
 	
 	for (i in categories) {
 		var category = categories[i];
-		
-		buildCategoryTrees(category.categories)
+		wrapper.append("<a href='photos?categoryId="+category.id+"'>"+category.name+"</a>");
+		wrapper.append(buildCategoryTrees(category.categories));
 	}
+	
+	return wrapper;
+}
+
+function getCategoryTree() {
+	$.getJSON( "categoryTree", {}, function( response ) {
+		return response.categories;
+	});
 }
